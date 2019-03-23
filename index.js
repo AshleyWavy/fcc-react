d1ad7cb4c12144b489491bb98b5bfc018dff4242
Ashley <>
2019-03-23 12:38:43 -0400

    Remove lines that import React and ReactDOM

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // Importing react libraries and tools using ES6 (es6 modules)
-import React from "react"
-import ReactDOM from "react-dom"
+//import React from "react"
+//import ReactDOM from "react-dom"
 
 // JSX
 ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
