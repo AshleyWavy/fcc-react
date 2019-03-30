bfae0548a60651a1a4bad16fc8b2662d913b460f
Ashley <>
2019-03-30 04:36:01 -0400

    Package of code

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-//ReactDOM & JSX Practice
 
-//Using ReactDOM library to render some JSX to the page
-//ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
-ReactDOM.render(
-  <ul>
+/*
+
+Functional component called MyApp
+
+Returns a single jsx element (being more analytical i see ul to ul..it wraps all inside)
+*/
+function MyApp() {
+  return (
+    <ul>
     <li>1</li>
     <li>2</li>
     <li>3</li>
-  </ul>,
+    </ul>
+  )
+}
+
+
+ReactDOM.render(
+  <MyApp />,
   document.getElementById("root")
 )
+
